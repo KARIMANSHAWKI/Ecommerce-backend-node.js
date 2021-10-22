3e430cef1972fe0ebf1cdc41cfd3df902b2a0fb0
KARIMANSHAWKI <kariman0800@gmail.com>
2021-10-22 03:06:01 +0200

    adding login and register api

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require('./routes/auth')
+
+dotenv.config();
+
 
 mongoose
-  .connect("mongodb+srv://admin:191998kokii@ecommerce.tioz4.mongodb.net/ecommerce?retryWrites=true&w=majority")
+  .connect(process.env.MONGOOSE_URL)
   .then(() => {
     console.log("database connection start ....");
   })
-  .catch((err)=> console.log(err));
+  .catch((err)=> console.log({error: err}));
 
-app.listen(3000, () => {
+app.use(express.json());
+
+//  ROUTES
+app.use('/api/auth', authRoute);
+
+
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("server start ....");
 });
