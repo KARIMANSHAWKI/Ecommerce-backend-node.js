caa16d1dbb3b38f52aa7af417cd30a51118df285
KARIMANSHAWKI <kariman0800@gmail.com>
2021-11-20 01:54:41 +0200

    seperate mongo config

--- index.js
+++ index.js
@@ -2,25 +2,37 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoute = require('./routes/auth')
+const cors = require('cors');
+const connectDB = require("./config/db");
 
+// load routes
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const productRoute = require('./routes/products');
+const cartRoute = require('./routes/carts');
+const orderRoute = require('./routes/orders');
+const paymentRout = require('./routes/stripe');
+
+
+// load config 
 dotenv.config();
 
+// connect to db
+connectDB();
 
-mongoose
-  .connect(process.env.MONGOOSE_URL)
-  .then(() => {
-    console.log("database connection start ....");
-  })
-  .catch((err)=> console.log({error: err}));
-
+// middleware
+app.use(cors());
 app.use(express.json());
 
-//  ROUTES
+// routes
 app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/checkout', paymentRout)
 
-
-
+// run server
 app.listen(process.env.PORT || 5000, () => {
   console.log("server start ....");
 });
